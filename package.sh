76e62ff78747f9e6b2c73bb233f65110c6d4f6ef
Nathan Brown <nbrown.us@gmail.com>
2013-12-04 17:49:22 -0800

    Adding 1.0.0 deb

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ make clean
 rm -rf ./release
 MLOGGER_VERSION=`sed -n 's/.*(\(.\+\?\)).*/\1/p' debian/changelog | head -n1`
 mkdir -p ./release/source
-cp -R ./!(release|package.sh) ./release/source/
+cp -R ./!(release|package.sh|dist) ./release/source/
 cd ./release
 #Not building source releases right now
 #tar -czf "mlogger_$MLOGGER_VERSION.orig.tar.gz" -C ./source .
